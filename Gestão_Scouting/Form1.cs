da07dca26c2cc71fc536a13d581a0c8122f417c9
pedrod33 <pedrod33@ua.pt>
2020-06-08 18:03:22 +0100

    nada

--- Gestão_Scouting/Form1.cs
+++ Gestão_Scouting/Form1.cs
@@ -49,8 +49,9 @@ namespace Gestão_Scouting
         private SqlConnection getSGBDConnection()
         {
             //Local a Editar!!
-            return new SqlConnection("data source=LAPTOP-MH91MTBV;integrated security=true;initial catalog=Trabalho_Final");
+            return new SqlConnection("data source=LAPTOP-2KEGA0ER;integrated security=true;initial catalog=Proj");
             //MH91MTBV
+            //2KEGA0ER
         }
 
         private bool verifySGBDConnection()
