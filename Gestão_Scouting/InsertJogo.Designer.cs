50026186609b0acf6468265316de88b6a3a2880c
JoaoTS <47570885+JoaoTS@users.noreply.github.com>
2020-06-12 13:42:48 +0100

    Fix

--- Gestão_Scouting/InsertJogo.Designer.cs
+++ Gestão_Scouting/InsertJogo.Designer.cs
@@ -79,7 +79,6 @@
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(196, 22);
             this.dateTimePicker1.TabIndex = 3;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // textBoxCompID
             // 
