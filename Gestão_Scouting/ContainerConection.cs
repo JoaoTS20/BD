01787da09746854e959cd478396f414b653fc9cf
JoaoTS <47570885+JoaoTS@users.noreply.github.com>
2020-06-12 22:54:47 +0100

    Push Connection to Server Aula

--- Gestão_Scouting/ContainerConection.cs
+++ Gestão_Scouting/ContainerConection.cs
@@ -8,6 +8,8 @@ namespace Gestão_Scouting.Classes
 {
     public static class ContainerConection
     {
-        public static String Connection = "data source = LAPTOP-MH91MTBV; integrated security = true; initial catalog = Trabalho_Final_Video";
+        public static String ConnectionLocal = "data source = LAPTOP-MH91MTBV; integrated security = true; initial catalog = Trabalho_Final_Video";
+        public static String Connection = "Data Source =tcp:mednat.ieeta.pt\\SQLSERVER,8101;Initial Catalog = p6g7; uid = p6g7; password =1894925886@BD";
+
     }
 }
